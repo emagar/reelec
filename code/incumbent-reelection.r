20f4e8e27a6c9b332f8cc75c69de51c35d429de1
Eric Magar <emagar@gmail.com>
2020-10-03 10:02:34 -0500

    Fausse route, will restore v5 splits

--- code/incumbent-reelection.r
+++ code/incumbent-reelection.r
@@ -1451,12 +1451,13 @@ v5 <- rbind(v5,tmp)
 v5$v01 <- v5$v02 <- v5$v03 <- v5$v04 <- v5$v05 <- v5$v06 <- v5$v07 <- v5$v08 <- v5$v09 <- v5$v10 <- v5$v11 <- v5$v12 <- v5$v13 <- v5$v14 <- NULL
 v5$l01 <- v5$l02 <- v5$l03 <- v5$l04 <- v5$l05 <- v5$l06 <- v5$l07 <- v5$l08 <- v5$l09 <- v5$l10 <- v5$l11 <- v5$l12 <- v5$l13 <- v5$l14 <- NULL
 #
-v5$oth <- v5$morena <- v5$prd <- v5$pri <- v5$pan <- 0
+#v5$oth <- v5$morena <- v5$prd <- v5$pri <- v5$pan <- 0
+v5$oth <- v5$left <- v5$prd <- v5$pri <- v5$pan <- 0
 v5$dmajcoal <- 0 # will indicate major party coalitions
 #
 rm(tmp, tmp.orig)
 #
-# change prd/morena to left
+# change prd/morena to left labels
 sel <- which(v5$yr<=2015)
 v5$l[sel] <- sub("prd","left",v5$l[sel])
 sel <- which(v5$yr>=2015)
@@ -1477,27 +1478,36 @@ v5$status[sel] <- "majors"
 sel1 <- which(is.na(v5$status))
 sel <- grep("pan-|-pan|^pan$", v5$l[sel1])
 v5$pan[sel1][sel] <- v5$v[sel1][sel]
-v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
+v5$v[sel1][sel] <- 0;
+v5$l[sel1][sel] <- "0";
+v5$status[sel1][sel] <- "done"
 #
 sel1 <- which(is.na(v5$status))
 sel <- grep("pri-|-pri|^pri$", v5$l[sel1])
 v5$pri[sel1][sel] <- v5$v[sel1][sel]
-v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
+v5$v[sel1][sel] <- 0;
+v5$l[sel1][sel] <- "0";
+v5$status[sel1][sel] <- "done"
 #
 sel1 <- which(is.na(v5$status))
 sel <- grep("left-|-left|^left$", v5$l[sel1])
-v5$morena[sel1][sel] <- v5$v[sel1][sel]
-v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
+v5$left[sel1][sel] <- v5$v[sel1][sel]
+v5$v[sel1][sel] <- 0;
+v5$l[sel1][sel] <- "0";
+v5$status[sel1][sel] <- "done"
 #
 sel1 <- which(is.na(v5$status))
 sel <- grep("prd-|-prd|^prd$", v5$l[sel1])
 v5$prd[sel1][sel] <- v5$v[sel1][sel]
-v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
+v5$v[sel1][sel] <- 0;
+v5$l[sel1][sel] <- "0";
+v5$status[sel1][sel] <- "done"
 #
-sel1 <- which(is.na(v5$status))
-sel <- grep("morena-|-morena|^morena$", v5$l[sel1])
-v5$morena[sel1][sel] <- v5$v[sel1][sel]
-v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
+## # morena is all left now, has no column
+## sel1 <- which(is.na(v5$status))
+## sel <- grep("morena-|-morena|^morena$", v5$l[sel1])
+## v5$morena[sel1][sel] <- v5$v[sel1][sel]
+## v5$v[sel1][sel] <- 0; v5$l[sel1][sel] <- "0"; v5$status[sel1][sel] <- "done"
 #
 # rest are other
 sel1 <- which(is.na(v5$status)) 
@@ -1507,69 +1517,133 @@ v5$v[sel1] <- 0; v5$l[sel1] <- "0"; v5$status[sel1] <- "done"
 # 3-majors coalition in mun split in thirds
 sel <- which(v5$status=="majors") 
 sel1 <- grep("(?=.*pan)(?=.*pri)(?=.*left)", v5$l[sel], perl = TRUE) # 
-v5$pan[sel][sel1] <- v5$v[sel][sel1] / 3; v5$pri[sel][sel1] <- v5$v[sel][sel1] / 3; v5$prd[sel][sel1] <- v5$v[sel][sel1] / 3; v5$v[sel][sel1] <- 0; v5$l[sel][sel1] <- "0"; v5$status[sel][sel1] <- "done"
+v5$pan[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$pri[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$left[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$v[sel][sel1] <- 0;
+v5$l[sel][sel1] <- "0";
+v5$status[sel][sel1] <- "done"
 v5$dmajcoal[sel][sel1] <- 1
 #
 sel <- which(v5$status=="majors") 
 sel1 <- grep("(?=.*pan)(?=.*pri)(?=.*prd)", v5$l[sel], perl = TRUE) # 
-v5$pan[sel][sel1] <- v5$v[sel][sel1] / 3; v5$pri[sel][sel1] <- v5$v[sel][sel1] / 3; v5$prd[sel][sel1] <- v5$v[sel][sel1] / 3; v5$v[sel][sel1] <- 0; v5$l[sel][sel1] <- "0"; v5$status[sel][sel1] <- "done"
+v5$pan[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$pri[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$prd[sel][sel1] <- v5$v[sel][sel1] / 3;
+v5$v[sel][sel1] <- 0;
+v5$l[sel][sel1] <- "0";
+v5$status[sel][sel1] <- "done"
 v5$dmajcoal[sel][sel1] <- 1
 #
 # pan-pri to pri (19 cases in mic07 mic11 mic15)
 sel <- which(v5$status=="majors" & v5$edon==16) 
 sel1 <- grep("(?=.*pan)(?=.*pri)", v5$l[sel], perl = TRUE) # 
-v5$pri[sel][sel1] <- v5$v[sel][sel1]; v5$v[sel][sel1] <- 0; v5$l[sel][sel1] <- "0"; v5$status[sel][sel1] <- "done"
+v5$pri[sel][sel1] <- v5$v[sel][sel1];
+v5$v[sel][sel1] <- 0;
+v5$l[sel][sel1] <- "0";
+v5$status[sel][sel1] <- "done"
 v5$dmajcoal[sel][sel1] <- 1
 #
 # pri-prd to pri (chihuahua and guanajuato)
 sel <- which(v5$status=="majors") 
 sel1 <- grep("(?=.*pri)(?=.*left)", v5$l[sel], perl = TRUE) # 
-v5$pri[sel][sel1] <- v5$v[sel][sel1]; v5$v[sel][sel1] <- 0; v5$l[sel][sel1] <- "0"; v5$status[sel][sel1] <- "done"
+v5$pri[sel][sel1] <- v5$v[sel][sel1];
+v5$v[sel][sel1] <- 0;
+v5$l[sel][sel1] <- "0";
+v5$status[sel][sel1] <- "done"
+v5$dmajcoal[sel][sel1] <- 1
+#
+# pri-prd to pri (chihuahua and guanajuato)
+sel <- which(v5$status=="majors") 
+sel1 <- grep("(?=.*pri)(?=.*prd)", v5$l[sel], perl = TRUE) # 
+v5$pri[sel][sel1] <- v5$v[sel][sel1];
+v5$v[sel][sel1] <- 0;
+v5$l[sel][sel1] <- "0";
+v5$status[sel][sel1] <- "done"
 v5$dmajcoal[sel][sel1] <- 1
 #
 # rest are pan-prd
 #
 # pan-prd to pan (bc coa2009 coa col00 col18 cua dgo jal que san sin son tam yuc)
 sel <- which(v5$status=="majors" & (v5$edon==2 | v5$edon==5 | v5$edon==6 | v5$edon==8 | v5$edon==10 | v5$edon==14 | v5$edon==22 | v5$edon==24 | v5$edon==25 | v5$edon==26 | v5$edon==28  | v5$edon==31)) 
-v5$pan[sel] <- v5$v[sel]; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$pan[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 # pan-prd in 2018 to pan (bcs cps df gue mex mic oax pue qui tab zac)
 sel <- which(v5$status=="majors" & v5$yr==2018) 
-v5$pan[sel] <- v5$v[sel]; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$pan[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 # pan-prd to prd (cps2004, cps2010)
 sel <- which(v5$status=="majors" & v5$edon==7 & v5$yr<=2010) 
-v5$prd[sel] <- v5$v[sel]; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$left[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 # pan-prd to pan (nay1999 nay2017 ver2000 ver2017)
 sel <- which(v5$status=="majors" & (v5$edon==18 | v5$edon==30) & (v5$yr==1999 | v5$yr==2000 | v5$yr==2017)) 
-v5$pan[sel] <- v5$v[sel]; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$pan[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 # pan-prd split halfway (qui2016)
 sel <- which(v5$status=="majors" & v5$edon==23 & v5$yr==2016)
 v5$pan[sel] <- v5$v[sel] / 2; 
-v5$prd[sel] <- v5$v[sel] / 2; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$prd[sel] <- v5$v[sel] / 2;
+v5$v[sel] <- 0; v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
-# pan-prd split halfway (pue2010 pue2013 qui2013 qui2010)
-sel <- which(v5$status=="majors" & (v5$edon==21 | v5$edon==23))
-v5$pan[sel] <- v5$v[sel]; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+# pan-prd to pan (pue2010 pue2013)
+sel <- which(v5$status=="majors" & v5$edon==21)
+v5$pan[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
-# pan-prd split halfway (gue2015 hgo2011 mic2015 oax2010 oax2013 oax2016 zac2013, zac2016)
-sel <- which(v5$status=="majors" & (v5$edon==12 | v5$edon==13 | v5$edon==16 | v5$edon==20 | v5$edon==32) & v5$yr<2018) 
+# pan-prd to prd (qui2013 qui2010)
+sel <- which(v5$status=="majors" & v5$edon==23)
+v5$left[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
+v5$dmajcoal[sel] <- 1
+#
+# pan-prd to prd (gue2015 oax2016)
+sel <- which(v5$status=="majors" & (v5$edon==12 | v5$edon==20) & (v5$yr==2015 | v5$yr==2016)) 
+v5$prd[sel] <- v5$v[sel];
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
+v5$dmajcoal[sel] <- 1
+#
+# pan-prd to left (hgo2011 mic2015 oax2010 oax2013 zac2013, zac2016)
+sel <- which(v5$status=="majors" & (v5$edon==13 | v5$edon==16 | v5$edon==20 | v5$edon==32) & v5$yr<2018) 
 v5$pan[sel] <- v5$v[sel] / 2; 
-v5$prd[sel] <- v5$v[sel] / 2; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$left[sel] <- v5$v[sel] / 2;
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 # pan-prd split halfway (mex2006)
 sel <- which(v5$status=="majors" & v5$edon==15 & v5$yr==2006) 
 v5$pan[sel] <- v5$v[sel] / 2; 
-v5$prd[sel] <- v5$v[sel] / 2; v5$v[sel] <- 0; v5$l[sel] <- "0"; v5$status[sel] <- "done"
+v5$left[sel] <- v5$v[sel] / 2;
+v5$v[sel] <- 0;
+v5$l[sel] <- "0";
+v5$status[sel] <- "done"
 v5$dmajcoal[sel] <- 1
 #
 ## # used to check by hand
